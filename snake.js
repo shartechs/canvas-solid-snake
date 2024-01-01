0b095f9fd99b6b740b9949779dfb2492405529bd
Shartechs <shartechs@gmail.com>
2024-01-01 16:24:42 +0400

    added food and snake contexts

--- snake.js
+++ snake.js
@@ -1,8 +1,17 @@
+//board
 const blockSize = 25;
 const rows = 20;
 const cols = 20;
 const board = document.getElementById("board");
 const context = board.getContext("2d");
+
+//snake head
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+//food
+let foodX = blockSize * 10;
+let foodY = blockSize * 10;
 
 window.onload = () => {
   board.width = cols * blockSize;
@@ -14,4 +23,10 @@ window.onload = () => {
 const update = () => {
   context.fillStyle = "black";
   context.fillRect(0, 0, board.width, board.height);
+
+  context.fillStyle = "lime";
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  context.fillStyle = "red";
+  context.fillRect(foodX, foodY, blockSize, blockSize);
 };
